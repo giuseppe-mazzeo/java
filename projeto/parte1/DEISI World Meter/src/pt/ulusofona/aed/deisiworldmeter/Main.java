506d30880df599b59ff3ec711312434b52fb9223
giuseppe-mazzeo-a22204542 <giumazz03@gmail.com>
2024-03-27 14:22:35 +0000

    troca na funcao parseFile

--- projeto/parte1/DEISI World Meter/src/pt/ulusofona/aed/deisiworldmeter/Main.java
+++ projeto/parte1/DEISI World Meter/src/pt/ulusofona/aed/deisiworldmeter/Main.java
@@ -17,155 +17,62 @@ public class Main {
     }
 
      */
-
-
-    public static boolean lerFicheiroPaises(File folder) {
-        String[] dados = new String[4];
-        int linhaOK = 0;
-        int linhaNOK = 0;
-        int primeiraLinhaIncorreta = -1;
-        String[] separador;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(folder))) {
-            br.readLine();
-            String linhaAtual;
-
-            while ((linhaAtual = br.readLine()) != null) {
-                separador = linhaAtual.split(",");
-
-                if (separador.length != 4) {
-                    if (primeiraLinhaIncorreta == -1) {
-                        primeiraLinhaIncorreta = linhaOK + 1;
-                    }
-                    linhaNOK++;
-                    continue;
-                }
-
-                dados[0] = separador[0];
-                dados[1] = separador[1];
-                dados[2] = separador[2];
-                dados[3] = separador[3];
-
-                linhaOK++;
-                baseDado.add(dados);
-                dados = new String[4];
-            }
-        } catch (IOException e) {
-            return false;
-        }
-
-        leituraLinhas[0] = linhaOK;
-        leituraLinhas[1] = linhaNOK;
-        leituraLinhas[2] = primeiraLinhaIncorreta;
-        System.out.println(Arrays.toString(leituraLinhas));
-
-        return true;
-    }
-
-
-    public static boolean lerFicheiroCidades(File folder) {
-        String[] dados = new String[6];
-        int linhaOK = 0;
-        int linhaNOK = 0;
-        int primeiraLinhaIncorreta = -1;
-        String[] separador;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(folder))) {
-            br.readLine();
-            String linhaAtual;
-
-            while ((linhaAtual = br.readLine()) != null) {
-                separador = linhaAtual.split(",");
-
-                if (separador.length != 6) {
-                    if (primeiraLinhaIncorreta == -1) {
-                        primeiraLinhaIncorreta = linhaOK + 1;
-                    }
-                    linhaNOK++;
-                    continue;
-                }
-
-                dados[0] = separador[0];
-                dados[1] = separador[1];
-                dados[2] = separador[2];
-                dados[3] = separador[3];
-                dados[4] = separador[4];
-                dados[5] = separador[5];
-
-                linhaOK++;
-                baseDado.add(dados);
-                dados = new String[6];
-            }
-
-        } catch (IOException e) {
-            return false;
-        }
-
-        leituraLinhas[0] = linhaOK;
-        leituraLinhas[1] = linhaNOK;
-        leituraLinhas[2] = primeiraLinhaIncorreta;
-        System.out.println(Arrays.toString(leituraLinhas));
-
-        return true;
-    }
-
-
-    static public boolean lerFicheiroPopulacao(File folder) {
-        String[] dados = new String[5];
-        int linhaOK = 0;
-        int linhaNOK = 0;
-        int primeiraLinhaIncorreta = -1;
-        String[] separador;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(folder))) {
-            br.readLine();
-            String linhaAtual;
-
-            while ((linhaAtual = br.readLine()) != null) {
-                separador = linhaAtual.split(",");
-
-                if (separador.length != 5) {
-                    if (primeiraLinhaIncorreta == -1) {
-                        primeiraLinhaIncorreta = linhaOK + 1;
-                    }
-                    linhaNOK++;
-                    continue;
-                }
-
-                dados[0] = separador[0];
-                if ((dados[1] = separador[1]).equals("Medium")) dados[1] = "";
-                dados[2] = separador[2];
-                dados[3] = separador[3];
-                dados[4] = separador[4];
-
-                linhaOK++;
-                baseDado.add(dados);
-                dados = new String[5];
-            }
-
-        } catch (IOException e) {
-            return false;
-        }
-
-        leituraLinhas[0] = linhaOK;
-        leituraLinhas[1] = linhaNOK;
-        leituraLinhas[2] = primeiraLinhaIncorreta;
-        System.out.println(Arrays.toString(leituraLinhas));
-
-        return true;
-    }
-
-
+    
     public static boolean parseFiles(File folder) {
+        String[] dados;
+        int linhaOK = 0;
+        int linhaNOK = 0;
+        int primeiraLinhaIncorreta = -1;
+        int tamanhoArray;
+        String[] separador;
+
         if (folder.getName().equals("paises.csv")) {
-            return lerFicheiroPaises(folder);
-        } else if (folder.getName().equals("cidades.csv")) {
-            return lerFicheiroCidades(folder);
+            tamanhoArray = 4;
         } else if (folder.getName().equals("populacao.csv")) {
-            return lerFicheiroPopulacao(folder);
+            tamanhoArray = 5;
+        } else if (folder.getName().equals("cidades.csv")) {
+            tamanhoArray = 6;
         } else {
             return false;
         }
+
+        dados = new String[tamanhoArray];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(folder))) {
+            br.readLine();
+            String linhaAtual;
+
+            while ((linhaAtual = br.readLine()) != null) {
+                separador = linhaAtual.split(",");
+
+                if (separador.length != tamanhoArray) {
+                    if (primeiraLinhaIncorreta == -1) {
+                        primeiraLinhaIncorreta = linhaOK + 1;
+                    }
+                    linhaNOK++;
+                    continue;
+                }
+
+                for (int i = 0; i < tamanhoArray; i++) {
+                    dados[i] = separador[i];
+                    if ((dados[1] = separador[1]).equals("Medium")) dados[1] = "";
+                }
+
+                linhaOK++;
+                baseDado.add(dados);
+                dados = new String[tamanhoArray];
+            }
+
+        } catch (IOException e) {
+            return false;
+        }
+
+        leituraLinhas[0] = linhaOK;
+        leituraLinhas[1] = linhaNOK;
+        leituraLinhas[2] = primeiraLinhaIncorreta;
+        System.out.println(Arrays.toString(leituraLinhas));
+
+        return true;
     }
 
 
